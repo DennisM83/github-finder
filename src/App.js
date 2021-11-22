744d92609a953e3ca67dca8ad1c14a98751e3254
Dennis Wenrich <dennisfinley@gmail.com>
2021-11-21 23:08:53 -0500

    update App to class from functional

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h1>Hello From React</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello From React</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
