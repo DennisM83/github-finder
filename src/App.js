b656f1db06d9d755b48495261f8de4b4e20f7512
Dennis Wenrich <dennisfinley@gmail.com>
2021-11-24 14:36:01 -0500

    added Navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello From React</h1>
+        <Navbar />
       </div>
     );
   }
