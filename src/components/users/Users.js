c5c519ece8f8f20be852540ebc04658617c6de93
Dennis Wenrich <dennisfinley@gmail.com>
2021-11-26 12:25:06 -0500

    update to the style of the user component

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -26,13 +26,19 @@ class Users extends Component {
     }
     render() {
         return (
-            <div>
+            <div style={userGrid}>
                 {this.state.users.map(user => (
                     <UserItem key={user.id} user={user}/>
                 ))}
             </div>
         )
     }
+}
+
+const userGrid = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridGap: '1rem'
 }
 
 export default Users
