eb49876873235111e7f86ae29bfe9787cb88f96c
Dennis Wenrich <dennisfinley@gmail.com>
2021-11-28 16:32:38 -0500

    added PropTypes on component

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,6 +1,8 @@
 import React from "react";
 import UserItem from "./UserItem";
 import Spinner from "../layout/Spinner";
+import PropTypes from 'prop-types'
+
 
 const Users = ({ users, loading }) => {
   if (loading) {
@@ -16,10 +18,17 @@ const Users = ({ users, loading }) => {
   }
 };
 
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
+}
+
 const userGrid = {
   display: "grid",
   gridTemplateColumns: "repeat(3, 1fr)",
   gridGap: "1rem",
 };
+
+
 
 export default Users;
