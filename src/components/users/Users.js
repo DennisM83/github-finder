c6e1e3e286ca5c643a9a2c78d783443e7a18b5a3
Dennis Wenrich <dennisfinley@gmail.com>
2021-11-28 16:29:50 -0500

    added spinner to users component

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,15 +1,20 @@
 import React from "react";
 import UserItem from "./UserItem";
+import Spinner from "../layout/Spinner";
 
 const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userGrid}>
-        {users.map(user => (
+        {users.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
-}
+  }
+};
 
 const userGrid = {
   display: "grid",
